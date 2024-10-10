945ea55deb1cd71b87b60b452c8dc23851c0db37
JustNormalProgrammer <testkonto1290@gmail.com>
2024-10-10 17:48:35 +0200

    Add adding todo items functionality.
    Todo items's priority is now visualized with
    green/yello/red circles. Due dates are now
    formatted to mach dd/mm/yyyy format.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.css';
+import {format} from 'date-fns';
 import addBtnImg from './assets/add-ellipse-svgrepo-com.svg'
+import greenCircle from './assets/priority/green-circle-svgrepo-com.svg'
+import yellowCircle from './assets/priority/yellow-circle-svgrepo-com.svg'
+import redCircle from './assets/priority/red-circle-svgrepo-com.svg'
 
 class TodoItem {
     static id = 0;
@@ -21,7 +25,6 @@ class TodoItem {
         this.priority = priority;
         this.notes = notes;
     }
-
 }
 class Project {
     static id = 0;
@@ -71,13 +74,17 @@ function seedHelper() {
     for (let i = 0; i < 5; i++) {
         let project = new Project(`Project ${i}`);
         for (let j = 0; j < 5; j++) {
-            let todo = new TodoItem(`Title:${i}`, `Description:${i}`, Date.now(), i, "notes!");
+            let todo = new TodoItem(`Title:${i}`, `Description:${i}`, Date.now(), Math.floor(Math.random()*3+1));
             project.addTodo(todo);
         }
         projects.addProject(project);
     }
 }
 const todoController = (function () {
+
+    
+    const dialog = document.querySelector('#todo-dialog');
+    
     function createTodoCard(todo) {
         const card = document.createElement('div');
         card.classList.add('todo-item');
@@ -96,62 +103,66 @@ const todoController = (function () {
         divInfo.appendChild(divHeader);
         divInfo.appendChild(divDesc);
         const divDate = document.createElement('div');
-        const divPriority = document.createElement('div');
-        divDate.textContent = todo.dueDate;
-        divPriority.textContent = todo.priority;
+        const priority = document.createElement('img');
+        divDate.textContent = `Due date: ${format(todo.dueDate, 'MM/dd/yyyy')}`;
         divDate.classList.add('todo-info-last');
-        divPriority.classList.add('todo-info-last');
+        let priorityImgArr = [greenCircle, yellowCircle, redCircle];
+        priority.src = priorityImgArr[todo.priority-1];
+        priority.classList.add('todo-priority');
         card.appendChild(checkbox);
         card.appendChild(divInfo);
         card.appendChild(divDate);
-        card.appendChild(divPriority);
+        card.appendChild(priority);
         return card;
     }
-    function buttonHandler(project){
-        const dialog = document.querySelector('#todo-dialog');
-        dialog.showModal();
-    }
-    function createAddTodoButton(project){
+    
+    function createAddTodoButton(){
         const button = document.createElement('button');
         button.classList.add('add-todo-btn');
         const img = document.createElement('img');
         img.src= addBtnImg;
         button.appendChild(img);
         button.addEventListener('click', ()=>{
-            buttonHandler(project);
+            dialog.showModal();
         })
         return button;
     }
     return { createTodoCard, createAddTodoButton};
 })();
+
 const projectDisplayController = (function () {
     const addBtn = document.querySelector('.add-btn');
     const dialog = document.querySelector('#project-dialog');
-    const form = document.querySelector('.add-form')
+    const formProject = document.querySelector('#project-form');
     const ul = document.querySelector('.project-list');
     const projectHeader = document.querySelector('.project-header');
     const main = document.querySelector('main');
 
+    let activeProject = null;
+    function getActiveProject() {
+        return activeProject;
+    }
     addBtn.addEventListener('click', () => {
         dialog.showModal();
     })
-    form.addEventListener('submit', (e) => {
+    formProject.addEventListener('submit', (e) => {
         e.preventDefault();
         const input = document.querySelector('#prj-name')
         let prjName = input.value;
         const project = new Project(prjName);
         projects.addProject(project);
         ul.appendChild(createProjectCard(project));
-        input.value = '';
+        formProject.reset();    
         dialog.close();
     })
+    
     function createProjectCard(project) {
         const li = document.createElement('li');
         const button = document.createElement('button');
 
         button.addEventListener('click', () => {
+            activeProject = project;
             displayActiveProject(project);
-
         });
         button.classList.add('list-btn');
         project.getStatus ? button.classList.add('list-btn-done') : button.classList.add('list-btn-todo');
@@ -159,38 +170,56 @@ const projectDisplayController = (function () {
         li.appendChild(button);
         return li;
     }
-    function displayActiveProject(project) {
+    function displayActiveProject() {
         const todoDisplayed = document.querySelector('.todo-list');
-        displayActiveProjectHeader(project);
+        displayActiveProjectHeader();
         while (todoDisplayed.firstChild) {
             todoDisplayed.removeChild(todoDisplayed.firstChild);
         }
-        for (let todo of project.todoList) {
+        for (let todo of activeProject.todoList) {
             todoDisplayed.appendChild(todoController.createTodoCard(todo));
         }
-        main.appendChild(todoController.createAddTodoButton(project));
+        main.appendChild(todoController.createAddTodoButton());
     }
     
-    function displayActiveProjectHeader(project) {
-        projectHeader.textContent = project.name;
+    function displayActiveProjectHeader() {
+        projectHeader.textContent = activeProject.name;
     }
     function displayProjects() {
         while (ul.firstChild) {
             ul.removeChild(ul.firstChild);
         }
         if (projects.projectList[0]) {
-            displayActiveProject(projects.projectList[2])
+            activeProject = projects.projectList[0];
+            displayActiveProject()
         }
         for (let project of projects.projectList) {
             const card = createProjectCard(project);
             ul.appendChild(card);
         }
     }
-    return { displayProjects };
+    return { displayProjects, getActiveProject, displayActiveProject};
 })();
 
 const ScreenController = (function () {
     const projectDisplay = projectDisplayController;
+    const formTodo = document.querySelector('#todo-form');
+    const dialog = document.querySelector('#todo-dialog');
     seedHelper();
     projectDisplay.displayProjects();
+
+    formTodo.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const title = document.querySelector('#todo-title').value;
+        const desc = document.querySelector('#todo-desc').value;
+        const date = document.querySelector('#todo-dueDate').valueAsDate;
+        const priority = document.querySelector('input[name="todo-priority"]:checked').value;
+        const todo = new TodoItem(title,desc,date,priority);
+        formTodo.reset();
+        let project = projectDisplay.getActiveProject();
+        project.addTodo(todo);
+        projectDisplay.displayActiveProject();
+        dialog.close();
+    })
+
 })();
