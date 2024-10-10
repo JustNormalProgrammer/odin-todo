faac8065f4e7a12d1956fb01cf20b4f39bb07431
JustNormalProgrammer <testkonto1290@gmail.com>
2024-10-10 14:11:57 +0200

    Add button to add todo item to the project.
    Clicking the newly added button now shows dialog
    that has inputs to add new todo item to the
    project.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import './style.css';
-
+import addBtnImg from './assets/add-ellipse-svgrepo-com.svg'
 
 class TodoItem {
     static id = 0;
     isComplete = false;
-    constructor(title, desc,dueDate,priority){
+    constructor(title, desc, dueDate, priority) {
         this.id = TodoItem.id++;
         this.title = title;
         this.desc = desc;
         this.dueDate = dueDate;
-        this.priority = priority; 
+        this.priority = priority;
     }
-    switchStatus(){
+    switchStatus() {
         this.isComplete = !this.isComplete;
     }
-    edit(title, desc,dueDate,priority){
+    edit(title, desc, dueDate, priority) {
         this.title = title;
         this.desc = desc;
         this.dueDate = dueDate;
-        this.priority = priority; 
+        this.priority = priority;
         this.notes = notes;
     }
 
@@ -26,25 +26,25 @@ class TodoItem {
 class Project {
     static id = 0;
     isDone = false;
-    constructor(name){
+    constructor(name) {
         this.name = name;
         this.todoList = [];
         this.id = Project.id++;
     }
-    addTodo(todoItem){
-        if(!todoItem instanceof TodoItem){
+    addTodo(todoItem) {
+        if (!todoItem instanceof TodoItem) {
             console.log('Unexpected item.');
             return -1;
         }
         this.todoList.push(todoItem);
         return 1;
     }
-    removeTodo(todoItemIdx){
+    removeTodo(todoItemIdx) {
         this.todoList.splice(todoItemIdx, 1);
     }
-    getStatus(){
-        for(let todo of this.todoList){
-            if(todo.isComplete !== true) {
+    getStatus() {
+        for (let todo of this.todoList) {
+            if (todo.isComplete !== true) {
                 return false;
             }
         }
@@ -52,36 +52,33 @@ class Project {
     }
 }
 class ProjectList {
-    constructor(){
+    constructor() {
         this.projectList = [new Project("Initial list")];
     }
-    addProject(project){
-        if(!project instanceof Project){
-            console.log('Unexpected item.');
-            return -1;
+    addProject(project) {
+        if (!project instanceof Project) {
+            throw new Error('Unexpected item.');
         }
         this.projectList.push(project);
         return 1;
     }
-    remove(projectId){
-        this.projectList.splice(projectId,1);
+    remove(projectId) {
+        this.projectList.splice(projectId, 1);
     }
 }
 const projects = new ProjectList();
-function seedHelper(){
-    for(let i = 0; i < 5; i++){
+function seedHelper() {
+    for (let i = 0; i < 5; i++) {
         let project = new Project(`Project ${i}`);
-        for(let j = 0; j < 5; j++){
-            let todo = new TodoItem(`Title:${i}`,`Description:${i}`, Date.now(), i, "notes!");
+        for (let j = 0; j < 5; j++) {
+            let todo = new TodoItem(`Title:${i}`, `Description:${i}`, Date.now(), i, "notes!");
             project.addTodo(todo);
         }
-        projects.addProject(project); 
+        projects.addProject(project);
     }
-    console.log(projects);
 }
-const todoController = (function(){
-    function createTodoCard(todo){
-        
+const todoController = (function () {
+    function createTodoCard(todo) {
         const card = document.createElement('div');
         card.classList.add('todo-item');
         const checkbox = document.createElement('input');
@@ -110,71 +107,89 @@ const todoController = (function(){
         card.appendChild(divPriority);
         return card;
     }
-    return {createTodoCard};
+    function buttonHandler(project){
+        const dialog = document.querySelector('#todo-dialog');
+        dialog.showModal();
+    }
+    function createAddTodoButton(project){
+        const button = document.createElement('button');
+        button.classList.add('add-todo-btn');
+        const img = document.createElement('img');
+        img.src= addBtnImg;
+        button.appendChild(img);
+        button.addEventListener('click', ()=>{
+            buttonHandler(project);
+        })
+        return button;
+    }
+    return { createTodoCard, createAddTodoButton};
 })();
-const projectDisplayController = (function(){
+const projectDisplayController = (function () {
     const addBtn = document.querySelector('.add-btn');
-    const dialog = document.querySelector('dialog');
+    const dialog = document.querySelector('#project-dialog');
     const form = document.querySelector('.add-form')
     const ul = document.querySelector('.project-list');
-    const projectHeader = document.querySelector('.project-header')
-    function addEventListeners() {
-        addBtn.addEventListener('click', ()=>{
-            dialog.showModal();
-        })
-        form.addEventListener('submit', (e)=>{
-            e.preventDefault();
-            const input = document.querySelector('#prj-name')
-            let prjName = input.value;
-            const project = new Project(prjName);
-            projects.addProject(project);
-            ul.appendChild(createProjectCard(project));
-            input.value = '';
-            dialog.close();
-        })
-    }
-    function createProjectCard(project){
+    const projectHeader = document.querySelector('.project-header');
+    const main = document.querySelector('main');
+
+    addBtn.addEventListener('click', () => {
+        dialog.showModal();
+    })
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('#prj-name')
+        let prjName = input.value;
+        const project = new Project(prjName);
+        projects.addProject(project);
+        ul.appendChild(createProjectCard(project));
+        input.value = '';
+        dialog.close();
+    })
+    function createProjectCard(project) {
         const li = document.createElement('li');
         const button = document.createElement('button');
-        const todoDisplayed = document.querySelector('.todo-list');
+
         button.addEventListener('click', () => {
             displayActiveProject(project);
-            while(todoDisplayed.firstChild){
-                todoDisplayed.removeChild(todoDisplayed.firstChild);
-            }
-            for(let todo of project.todoList){
-                todoDisplayed.appendChild(todoController.createTodoCard(todo));
-            }
+
         });
         button.classList.add('list-btn');
-        project.getStatus ? button.classList.add('list-btn-done') : button.classList.add('list-btn-todo')
+        project.getStatus ? button.classList.add('list-btn-done') : button.classList.add('list-btn-todo');
         button.textContent = project.name;
         li.appendChild(button);
         return li;
     }
-    function displayActiveProject(project){
-        displayActvProjectHeader(project);
+    function displayActiveProject(project) {
+        const todoDisplayed = document.querySelector('.todo-list');
+        displayActiveProjectHeader(project);
+        while (todoDisplayed.firstChild) {
+            todoDisplayed.removeChild(todoDisplayed.firstChild);
+        }
+        for (let todo of project.todoList) {
+            todoDisplayed.appendChild(todoController.createTodoCard(todo));
+        }
+        main.appendChild(todoController.createAddTodoButton(project));
     }
-    function displayActvProjectHeader(project){
+    
+    function displayActiveProjectHeader(project) {
         projectHeader.textContent = project.name;
     }
-    function displayProjects(){
-        while(ul.firstChild){
+    function displayProjects() {
+        while (ul.firstChild) {
             ul.removeChild(ul.firstChild);
         }
-        if(projects.projectList[0]) {
-            displayActiveProject(projects.projectList[0])
+        if (projects.projectList[0]) {
+            displayActiveProject(projects.projectList[2])
         }
-        for(let project of projects.projectList){
+        for (let project of projects.projectList) {
             const card = createProjectCard(project);
             ul.appendChild(card);
         }
     }
-    addEventListeners();
-    return {displayProjects};
+    return { displayProjects };
 })();
 
-const ScreenController = (function(){
+const ScreenController = (function () {
     const projectDisplay = projectDisplayController;
     seedHelper();
     projectDisplay.displayProjects();
