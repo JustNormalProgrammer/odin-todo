ba12a1ac25c12e6f61e963c24211192b02940941
JustNormalProgrammer <testkonto1290@gmail.com>
2025-01-29 21:56:50 +0100

    Add docker files and docker compose setup

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const Project = require("./models/project");
 require("dotenv").config();
 
 const app = express();
